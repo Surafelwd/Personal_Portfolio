d63b6cd91e8445035cd9ae3a2cc79624a85f73e0
surafelwendnagegn457@gmail.com <your@email.com>
2025-09-17 11:23:54 +0300

    page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import MotionProvider from "@/components/MotionProvider";
 import Navbar from "@/components/Navbar";
-import Hero from "../components/Hero";
-import About from "../components/About";
-import Skills from "../components/Skills";
-import Projects from "../components/Projects";
-import Contact from "../components/Contact";
-import Services from "../components/Services";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
+import Services from "@/components/Services";
 export default function Home() {
   return (
     <MotionProvider>
